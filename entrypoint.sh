479c14448fd313a3c51517147d3d07cc3ad7d974
Zach Durham <zach.durham@k4connect.com>
2020-03-23 15:36:56 -0400

    fix section header

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ mkdir -p ~/.aws
 
 touch ~/.aws/credentials
 
-echo "[default
+echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
