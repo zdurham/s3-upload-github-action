161dfa6991b9d88a97f02f4aeb5dcd26ea7e03cd
Zach Durham <zach.durham@k4connect.com>
2020-03-23 15:54:23 -0400

    fix key

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ jobs:
     FILE: ./lambda.zip
     AWS_REGION: 'us-east-1'
     S3_BUCKET: ${{ secrets.AWS_S3_BUCKET }}
+    S3_KEY: ${{ secrets.S3_KEY }}
     AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
     AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${S3_BUCKET} \
+aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
   --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
