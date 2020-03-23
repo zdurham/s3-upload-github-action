fa75a852e51f57ec610f388888b8be5ce7629610
Zach Durham <zach.durham@k4connect.com>
2020-03-23 15:22:40 -0400

    adding checks and updating dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM python:3.7-alpine
 RUN pip install --quiet --no-cache-dir awscli
 
 ADD entrypoint.sh /entrypoint.sh
-
+RUN chmod +x /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,29 @@
 #!/bin/sh 
 
+set -e
+
+if [ -z "$S3_BUCKET" ]; then
+  echo "S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [-z "$FILE"]; then
+  echo "FILE is not set. Quitting"
+  exit 1
+fi
+
+if [ -z "$AWS_REGION"]; then
+  AWS_REGION="us-east-1"
+fi
+
 mkdir -p ~/.aws
 
 touch ~/.aws/credentials
@@ -12,3 +36,4 @@ aws s3 cp ${FILE} s3://${S3_BUCKET} \
   --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
+
