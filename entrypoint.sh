4c408ff80e5861cbff3d4b79dc60c7ee1b1c55ef
Zach Durham <zach.durham@k4connect.com>
2020-03-23 15:33:34 -0400

    trying without conditionals

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-if [ -z "$S3_BUCKET" ]; then
-  echo "S3_BUCKET is not set. Quitting."
-  exit 1
-fi
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
-fi
+# if [ -z "$S3_BUCKET" ]; then
+#   echo "S3_BUCKET is not set. Quitting."
+#   exit 1
+# fi
+# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+#   exit 1
+# fi
+# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+#   exit 1
+# fi
 
-if [-z "$FILE"]; then
-  echo "FILE is not set. Quitting"
-  exit 1
-fi
+# if [-z "$FILE"]; then
+#   echo "FILE is not set. Quitting"
+#   exit 1
+# fi
 
-if [ -z "$AWS_REGION"]; then
-  AWS_REGION="us-east-1"
-fi
+# if [ -z "$AWS_REGION"]; then
+#   AWS_REGION="us-east-1"
+# fi
 
 mkdir -p ~/.aws
 
